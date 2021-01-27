09688e83aaf87a607948413c9270409663169236
Nhuan Vu <nhuanvd@live.com>
2021-01-27 22:54:36 +0700

    Initial commit

---
+++ assets/configs.js
@@ -0,0 +1,20 @@
+const IMAGES = [
+  {
+    name: 'Test 1',
+    image: 'template/test1.jpg',
+    width: 500,
+    height: 500,
+    position: [120, 300],
+    fontSize: 12,
+    align: 'center|left|right'
+  },
+  {
+    name: 'Test 2',
+    image: 'template/test2.jpg',
+    width: 500,
+    height: 500,
+    position: [200, 50],
+    fontSize: 12,
+    align: 'center|left|right'
+  }
+]
